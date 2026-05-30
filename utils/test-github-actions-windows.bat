c7861dc07f8d51ce4df19e4446773b03dbd3ee4b
Chih-Hsuan Yen <1937689+yan12125@users.noreply.github.com>
2026-05-30 22:50:47 +0800

    Make CI running
    * Update actions for compatibility with the CI environment - cache
      server APIs, node.js versions, etc.
    * Update python version and CI commands for NodPA, see README.md
    * Drop stale access token. Submodules are all public now, so it is not
      needed anymore.

--- utils/test-github-actions-windows.bat
+++ utils/test-github-actions-windows.bat
@@ -1,7 +1,7 @@
 python3 -m pip install --user --upgrade pip setuptools
 python3 -m pip install -r requirements-base.txt
 
-python3 dnn-models\transform.py --data-output-dir build-windows --target msp430 --hawaii har
+python3 dnn-models\transform.py --data-output-dir build-windows --target msp432 --hawaii har-dnp
 cmake -B build-windows -G "Visual Studio 17 2022"
 cd build-windows
 msbuild intermittent-cnn.sln
--- utils/test-github-actions.sh
+++ utils/test-github-actions.sh
@@ -1,7 +1,7 @@
 python3 -m pip install --user --upgrade pip setuptools
 python3 -m pip install -r requirements-base.txt
 
-python3 dnn-models/transform.py --target msp430 --hawaii har
+python3 dnn-models/transform.py --target msp432 --hawaii har-dnp
 cmake -B build
 make -C build
 ./build/intermittent-cnn
